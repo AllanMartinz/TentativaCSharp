60b3dcd0646796d066a3989620a4b2998446d1cc
BanjooKazooie <129326867+BanjooKazooie@users.noreply.github.com>
2024-06-03 20:58:50 -0300

    03/06/24 - HUMBLE. Delegates

---
+++ SaimonCu/MetodosEFuncoes/DelegatesParametros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaimonCu.MetodosEFuncoes
+{
+    class DelegatesParametros
+    {
+        public delegate int Operacao(int x, int y);
+        public static int Soma(int x, int y)
+        {
+            return x + y;
+        }
+        public static string Calculadore(Operacao op, int x, int y)
+        {
+            var resultado = op(x, y);
+            return "RESULTADO: " + resultado;
+        }
+
+        public static void Executar()
+        {
+            Operacao subb = (int x, int y) => x - y;
+            Console.WriteLine(Calculadore(subb, 80, 456));
+
+            Console.WriteLine(Calculadore(Soma, 43, 34));
+        }
+    }
+}
